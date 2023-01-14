b2996168ba2e7d86d10cd9d1ce7cc06c408e97ef
Felipe Ugarte <felipe.ugarte.h@icloud.com>
2023-01-14 13:15:26 -0300

    Initial Commit

---
+++ TakeMyMoney/TakeMyMoney/TakeMyMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeMyMoneyApp.swift
+//  TakeMyMoney
+//
+//  Created by Felipe Ugarte on 14-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TakeMyMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
