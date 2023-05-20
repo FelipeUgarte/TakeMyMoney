3fb54219c5fc2148ef4450067ba79fc755fbc65a
Felipe <felipe.ugarte.h@icloud.com>
2023-05-20 13:36:36 -0400

    Update PaymentFormScreen.swift

--- TakeMyMoney/TakeMyMoney/PaymentForm/Screen/PaymentFormScreen.swift
+++ TakeMyMoney/TakeMyMoney/PaymentForm/Screen/PaymentFormScreen.swift
@@ -5,62 +5,11 @@ struct PaymentFormScreen: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(spacing: 24.0) {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 4.0) {
-                            Text("Total price")
-                                .font(.caption)
-
-                            Text("$\(viewModel.shoppingCart.total)")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundColor(.blue)
-                        }
-                        Spacer()
-                    }
-
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Payment Method")
-                                .font(.caption)
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack {
-                                    ForEach(viewModel.paymentData.paymentMethods.indices, id: \.self) { index in
-                                        let paymentMethod = viewModel.paymentData.paymentMethods[index]
-                                        PaymentOptionButton(
-                                            title: paymentMethod.title.rawValue,
-                                            state: paymentMethod.title == viewModel.selectedPaymentMethod
-                                        ) {
-                                            viewModel.selectedPaymentMethod = paymentMethod.title
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        Spacer()
-                    }
-
-                    // MARK: - Payment Details
-                    switch viewModel.selectedPaymentMethod {
-                        case .credit:
-                            CreditCardView(viewModel: viewModel)
-                        case .paypal:
-                            PayPalView()
-                        case .wallet:
-                            EmptyView()
-                    }
-                }
-                .padding()
-            }
-
-            Button {
-                viewModel.purchaseButtonAction()
-            } label: {
-                Text("Proceed to confirm")
-                    .padding()
-            }.buttonStyle(.borderedProminent)
-            .navigationTitle("Payment data")
+            PaymentFormView(selectedMethod: $viewModel.selectedPaymentMethod,
+                            total: viewModel.shoppingCart.total,
+                            methods: viewModel.paymentData.paymentMethods)
+            PaymentButton(action: viewModel.purchaseButtonAction)
+                .navigationTitle("Payment data")
         }
     }
 }
